97d7cb82de58d1deca031fbbe3495461c273237d
arthiatsr <arthiatsr@users.noreply.github.com>
2019-12-19 00:06:39 -0600

    Updates

---
+++ precache-manifest.a931299a98deed2d1c980211931b68a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e899fd973f39890d1c6be5bcb2da56bb",
+    "url": "/clickmegame/index.html"
+  },
+  {
+    "revision": "16b4f805dec6350c16f4",
+    "url": "/clickmegame/static/css/main.1db9acb1.chunk.css"
+  },
+  {
+    "revision": "558efa81d84a9519ed4e",
+    "url": "/clickmegame/static/js/2.35648e6a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickmegame/static/js/2.35648e6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16b4f805dec6350c16f4",
+    "url": "/clickmegame/static/js/main.39077d39.chunk.js"
+  },
+  {
+    "revision": "e013bf4e23684bc6946c",
+    "url": "/clickmegame/static/js/runtime-main.14901252.js"
+  }
+]);
